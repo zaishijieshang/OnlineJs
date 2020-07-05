5476a7879c516b876ced28d11ac8fabd4394116a
zaishijieshang <280402521@qq.com>
2020-07-05 15:55:22 +0800

    Add files via upload

---
+++ DSAuto2.js
@@ -0,0 +1,36 @@
+﻿function dsAuto() {
+    getSubDesc = function () {
+        return ".Subject-Description";
+    }
+    this.autoAnswerDefault = function (isSubmit) {
+        var items = getSubjectItems();
+        setDefaultAnswer(items);
+        if (isSubmit) {
+            $("#submitBtn").click();
+        }
+    }
+    this.getSubjectItems = function () {
+        var items = [];
+        $(subDesc).each(function (i, v) {
+            items.push($(v).attr('itemid'));
+        });
+        return items;
+    }
+    this.setDefaultAnswer = function (items) {
+        items.forEach(function (v) {
+            $("[itemid='" + v + "'] li").get(0).click();
+        });
+    }
+    //获取答案
+    this.getSubjectAnswer = function () {
+        var items = [];
+        $(subDesc).each(function (i, v) {
+            var dan = {};
+            dan.itemid = $(v).attr('itemid');
+            dan.val = $(v).find(".Teacher-Answer span").text();
+            items.push(dan);
+        });
+        return items;
+    }
+
+}
